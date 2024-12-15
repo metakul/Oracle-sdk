06fc45dffbb551a45322591f46ac6c1ddfee881b
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-12-15 21:16:24 +0530

    Add gitignore

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function addressToString(value: bigint): string;
-export declare function stripLeadingZeros(byteArray: Uint8Array): Uint8Array;
-export declare function unsignedIntegerToUint8Array(value: bigint | number, widthInBytes?: 8 | 20 | 32 | 256): Uint8Array<ArrayBuffer>;
